a270fa039255c2b85499be4b46366fbb934207dc
Alexander Marc Mantel <alexandermarc@bredex.de>
2024-12-04 17:01:44 +0100

    feat: inject products with working svg and add dialogs

--- src/js/app.js
+++ src/js/app.js
@@ -2,19 +2,28 @@
  * Injects HTML from a file into an element
  * @param {string} htmlFile - The path to the HTML file
  * @param {string} element - The element to inject the HTML into
- * @returns {void}
+ * @returns {Promise<void>}
  * @example
  * injectHTML('path/to/file.html', '.element')
  */
 function injectHTML(htmlFile, element) {
-  fetch(htmlFile)
+  return fetch(htmlFile)
     .then((response) => response.text())
     .then((data) => {
       document.querySelector(element).innerHTML = data;
-    });
+    })
+    .then(() => console.log(`Injected ${htmlFile} into ${element}`));
 }
 
 // Inject the header
-injectHTML("components/header/header.html", "#header");
-injectHTML("components/products/products.html", "#products");
-injectHTML("components/modals/new-product.html", "#modal__new-product");
+async function injectAll() {
+  await injectHTML("components/header/header.html", "#header");
+  await injectHTML("components/products/products.html", "#products");
+  await injectHTML("components/modals/new-product.html", "#modal__new-product");
+  await injectHTML(
+    "components/modals/edit-product.html",
+    "#modal__edit-product",
+  );
+}
+
+injectAll();
